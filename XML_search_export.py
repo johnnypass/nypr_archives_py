e4d751cd6bfb924d09354ea802218b60c3164010
johnnypass <jwpassmore@gmail.com>
2017-01-12 12:27:28 -0500

    Update XML_search_export.py

--- XML_search_export.py
+++ XML_search_export.py
@@ -6,37 +6,46 @@ This downloads archives records as XML documents based on filtered search result
 """
 
 import cookielib
-from getpass import getpass
 import os
 import re
 import sys
 import urllib
 import urllib2
 
-ARCHIVES_USERNAME = ('ARCHIVES_USERNAME', 'XXXXXXX')
+ARCHIVES_USERNAME = ''
+ARCHIVES_URL = 'https://cavafy.wnyc.org'
 SEARCH_URLS = [
     'ENTER SEARCH RESULTS URL HERE',
 ]
 
-
 def main(argv):
     xml_dir = argv[-1]
     if not os.path.isdir(xml_dir):
+        print(argv)
         print 'Usage: %s <dest dir>' % argv[0]
         return
-    password = ('ARCHIVES_SITE_PASSWORD', 'XXXXXXX')
-    if password is None:
-        password = getpass("Password for %s on cavafy: " % ARCHIVES_USERNAME)
+    password = ARCHIVES_SITE_PASSWORD
     opener = log_in_to_archives(ARCHIVES_USERNAME, password)
-    print "Downloading archives records to '%s'" % xml_dir
-    download_archives_data(opener, SEARCH_URLS, xml_dir)
+
+    if argv[1] == '--record':
+         download_xml_file('%s/assets/%s.xml' % (ARCHIVES_URL, argv[2]), opener, xml_dir)
+    else:
+        print "Downloading archives records to '%s'" % xml_dir
+        download_archives_data(opener, SEARCH_URLS, xml_dir)
 
 
 def log_in_to_archives(username, password):
     cj = cookielib.CookieJar()
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
     print "Logging in to archives database."
-    resp = opener.open('http://cavafy.wnyc.net').read()
+    resp = opener.open(ARCHIVES_URL).read()
+    token = re.search('type="hidden" value="([^"]+)"', resp).groups()[0]
+    login_data = urllib.urlencode({
+        'login': username, 'password': password,
+        'authenticity_token': token})
+    resp = opener.open(ARCHIVES_URL+'/session', login_data).read()
+    if 'Logged in as' not in resp:
+        raise Exception("Couldn't log in.")
     print "Logged in."
     return opener
 
@@ -46,11 +55,15 @@ def download_archives_data(opener, search_urls, xml_dir):
         for url in search_assets(opener, search_url):
             xml_url = url + '.xml'
             print "Downloading " + xml_url
-            file_name = xml_url.split('/')[-1]
-            xml_file = open(os.path.join(xml_dir, file_name), 'w')
-            xml = opener.open(xml_url).read()
-            xml_file.write(xml)
-            xml_file.close()
+            download_xml_file(xml_url, opener, xml_dir)
+
+
+def download_xml_file(xml_url, opener, xml_dir):
+    file_name = xml_url.split('/')[-1]
+    xml_file = open(os.path.join(xml_dir, file_name), 'w')
+    xml = opener.open(xml_url).read()
+    xml_file.write(xml)
+    xml_file.close()
 
 
 def search_assets(opener, url):
@@ -63,7 +76,7 @@ def search_assets(opener, url):
         if 'Nothing found' in html:
             break
         page += 1
-        asset_urls += re.findall('"(http://[^"]+/assets/[^"]+)"', html)
+        asset_urls += re.findall('"(https://[^"]+/assets/[^"]+)"', html)
     return asset_urls
 
 
